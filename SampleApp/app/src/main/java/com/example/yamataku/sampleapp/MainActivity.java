7be6e8f2fea75f6a65de04b011f9ad2a9e4b87f2
takuyamakawa <g2117052@fun.ac.jp>
2018-05-25 19:51:57 +0900

    各ボタンを各画面へ遷移するようにした

--- SampleApp/app/src/main/java/com/example/yamataku/sampleapp/MainActivity.java
+++ SampleApp/app/src/main/java/com/example/yamataku/sampleapp/MainActivity.java
@@ -22,5 +22,59 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button kodaiBtn = (Button) findViewById(R.id.kodai);
+        kodaiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Kodai.class);
+                startActivity(intent);
+            }
+        });
+
+        Button yukiBtn = (Button) findViewById(R.id.yuki);
+        yukiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Yuki.class);
+                startActivity(intent);
+            }
+        });
+
+        Button maayaBtn = (Button) findViewById(R.id.maaya);
+        maayaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Maaya.class);
+                startActivity(intent);
+            }
+        });
+
+        Button takamatuBtn = (Button) findViewById(R.id.takamatu);
+        takamatuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Takamatu.class);
+                startActivity(intent);
+            }
+        });
+
+        Button takenobuBtn = (Button) findViewById(R.id.takenobu);
+        takenobuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Takenobu.class);
+                startActivity(intent);
+            }
+        });
+
+        Button tomokaBtn = (Button) findViewById(R.id.tomoka);
+        tomokaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Tomoka.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ SampleApp/app/src/main/java/com/example/yamataku/sampleapp/Takenobu.java
@@ -0,0 +1,13 @@
+package com.example.yamataku.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Takenobu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takenobu);
+    }
+}
