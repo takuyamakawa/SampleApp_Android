37857769d11a20fa95a680789859fa3e77371c42
takuyamakawa <g2117052@fun.ac.jp>
2018-05-29 18:23:07 +0900

    add scrollView for introduction

--- SampleApp/app/src/main/java/com/example/yamataku/sampleapp/Takuya.java
+++ SampleApp/app/src/main/java/com/example/yamataku/sampleapp/Takuya.java
@@ -2,6 +2,7 @@ package com.example.yamataku.sampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Takuya extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class Takuya extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takuya);
+
+        TextView intro = findViewById(R.id.introduction);
+        String text = "ほげほげほげほげほげほげほげほげほげほげほげほげ" +
+                "ほげほげほげほげほげほげほげほげほげほげほげほげほげほげ" +
+                "ほげほげほげほげほげほげほげほげほげほげほげほげほげほげ" +
+                "ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ" +
+                "ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ" +
+                "ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ";
+
+        intro.setText(text);
     }
 }
