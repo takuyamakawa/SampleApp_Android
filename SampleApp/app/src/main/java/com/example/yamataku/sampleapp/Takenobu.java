f0963ec8e52dad8f5e8afe8dc27a1211170e8d17
Yuhei Takenobu <b1016127@fun.ac.jp>
2018-06-04 15:23:44 +0900

    リストクリック時に説明を表示

--- SampleApp/app/src/main/java/com/example/yamataku/sampleapp/Takenobu.java
+++ SampleApp/app/src/main/java/com/example/yamataku/sampleapp/Takenobu.java
@@ -2,9 +2,13 @@ package com.example.yamataku.sampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,18 +19,19 @@ public class Takenobu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takenobu);
 
+        HashMap<String, String> aMap = new HashMap<>();
 
-        Map<String, String> aMap = new HashMap<>();
-
-        //String Key[] = new String[10];
-
-
+        //情報をコードから入力
         aMap.put("ArrayAdapter","説明１");
         aMap.put("SimpleAdapter","説明２");
         aMap.put("BaseAdapter","説明３");
         aMap.put("a","説明４");
         aMap.put("b","説明５");
         aMap.put("c","説明６");
+        aMap.put("d","説明７");
+        for(int i=0; i<10; i++){
+            aMap.put("e"+i,"説明"+(i+7));
+        }
 
         ListView listView = (ListView) findViewById(R.id.dynamic);
 
@@ -36,7 +41,17 @@ public class Takenobu extends AppCompatActivity {
         for(String key : aMap.keySet()){
             adapter.add(key);
         }
-
         listView.setAdapter(adapter);
+
+        final HashMap<String, String> copyMap = aMap;
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView list = (ListView)parent;
+                String msg =  copyMap.get((String)list.getItemAtPosition(position));
+                Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
